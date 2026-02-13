bf005ed271c14fcc6e8ca56d2a4ef267471f0745
samlnz <82331715+samlnz@users.noreply.github.com>
2026-02-13 16:00:59 +0300

    Add files via upload

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,9 @@
-
-const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+const baseUrl = process.env.REACT_APP_API_URL || window.location.origin;
 
 export const config = {
   development: process.env.REACT_APP_DEVELOPMENT === "true",
   debug: true,
   appKey: "crash-0.1.0",
   api: `${baseUrl}/api`,
-  wss: baseUrl,
+  wss: baseUrl.replace(/^http/, 'ws'),
 };
