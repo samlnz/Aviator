c2626c8e348d0c9c6c501304d19c84a104c0e231
samlnz <82331715+samlnz@users.noreply.github.com>
2026-02-13 16:27:32 +0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -4,23 +4,23 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import './index.scss';
-import App from './src/app';
-import { Provider } from './src/context';
+import './index.css';
+import App from './app';
+import { Provider } from './context';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
 const root = createRoot(container);
 
 root.render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="*" element={
-				<Provider>
-					<App />
-					<ToastContainer position="top-center" theme="dark" />
-				</Provider>
-			} />
-		</Routes>
-	</BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider>
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
+        <ToastContainer position="top-center" theme="dark" />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
