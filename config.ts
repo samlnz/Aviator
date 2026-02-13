2244a082acd18cbde1790322d0a5b16cd3554f0e
samlnz <82331715+samlnz@users.noreply.github.com>
2026-02-13 16:53:37 +0300

    Remove duplicate config.ts from root

--- config.ts
+++
@@ -1,9 +0,0 @@
-const baseUrl = process.env.REACT_APP_API_URL || window.location.origin;
-
-export const config = {
-  development: process.env.REACT_APP_DEVELOPMENT === "true",
-  debug: true,
-  appKey: "crash-0.1.0",
-  api: `${baseUrl}/api`,
-  wss: baseUrl.replace(/^http/, 'ws'),
-};
